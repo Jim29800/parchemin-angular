af038c54a76e50fd2b47702ba60bb9669320254a
jim29800 <jbouvier1173@gmail.com>
2018-10-13 00:51:37 +0200

    envoi d'image en base64

--- src/app/product/product-new/product-new.component.ts
+++ src/app/product/product-new/product-new.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../../services/category/category.service';
 import { CityService } from '../../services/city/city.service';
 import { TagService } from '../../services/tag/tag.service';
+import { ProductService } from '../../services/product/product.service';
+
+let fileUpload = require('fuctbase64');
 
 @Component({
   selector: 'app-product-new',
@@ -14,7 +17,53 @@ export class ProductNewComponent implements OnInit {
   cities : object = [{ id: 0, name: "Récupération des villes en cours..." }];
   tags : object =[{ id: 0, name: "Récupération des tags en cours..." }];
 
-  constructor(public CategoryService: CategoryService, public CityService: CityService, public TagService: TagService) { }
+  category : string;
+  city : string;
+  tagSelected = [] ;
+
+  title : string;
+  ref : string;
+  description : string;
+
+  selectedFile: File;
+  fileResult: any;
+
+  
+  constructor(
+    public CategoryService: CategoryService, 
+    public CityService: CityService, 
+    public TagService: TagService,
+    public ProductService: ProductService
+    ) { }
+  
+  postProduct(){
+    let tags = [];
+    for (let i = 0; i < this.tagSelected.length; i++) {
+      if (this.tagSelected[i] == true) {
+        tags.push(i);
+      }
+      
+    }
+    const uploadData = new FormData();
+    uploadData.append('imageFile', this.selectedFile)
+
+    console.log(this.fileResult['__zone_symbol__value'])
+    this.ProductService.post(this.title, this.description, this.ref, this.category, this.city, tags, this.fileResult['__zone_symbol__value'])
+      .subscribe(event => {
+        // console.log("event : " + event);
+      },
+        error => {
+          // console.log(error);
+        }
+      );
+
+  }
+
+
+  onFileChange(event) {
+    let result = fileUpload(event);
+    this.fileResult = result;
+  }
 
   ngOnInit() {
     this.CategoryService.getCategories().subscribe(
--- src/app/services/product/product.service.ts
+++ src/app/services/product/product.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Globals } from '../../globals';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  token = localStorage.getItem('token');
+
+  header: HttpHeaders = new HttpHeaders({
+    'Content-Type': 'application/json',
+    "Authorization": "Bearer " + this.token,
+  });
+
+  constructor(private globals: Globals, private http: HttpClient) { }
+
+  post(title, description, ref, category, city, tags, image) {
+    let url: string = this.globals.url + "products";
+
+    return this.http.post(url,
+      {
+        "title": title,
+        "description": description,
+        "ref": ref,
+        "category": category,
+        "city": city,
+        "tags": tags,
+        "img" : image["base64"]
+        
+      },
+      {
+        headers: this.header
+      })
+      // .pipe(
+      //   map(data =>
+      //     console.log(data)
+      //   )
+      // )
+      ;
+  }
 }
