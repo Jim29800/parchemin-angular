6bf266b38fec82e6429c5b13613b48252fe13dde
jim29800 <jbouvier1173@gmail.com>
2018-10-14 20:08:09 +0200

    url de l'image

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -3,4 +3,5 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class Globals {
     url: string = 'http://127.0.0.1:8000/api/';
+    urlImage: string = "http://127.0.0.1:8000/uploads/images/products/";
 }
