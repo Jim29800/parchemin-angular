9c911d6e85e0129f81556d8daa05c94d0bdf9ff1
jim29800 <jbouvier1173@gmail.com>
2018-12-05 11:58:16 +0100

    fix

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -2,6 +2,6 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Globals {
-    url: string = 'http://127.0.0.1:8000/api/';
-    urlImage: string = "http://127.0.0.1:8000/uploads/images/products/";
+    url: string = 'https://vitrine.leparchemin.org/api/';
+    urlImage: string = "https://vitrine.leparchemin.org/uploads/images/products/";
 }
